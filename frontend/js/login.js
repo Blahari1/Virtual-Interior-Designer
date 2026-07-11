fe07c7898860e81235eba11070b640e835606163
Blahari1 <llahari150@gmail.com>
2026-07-11 11:06:10 +0530

    Change API_URL to new deployment endpoint
    Updated API_URL to point to the new deployment URL.

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -3,7 +3,7 @@
    Login Authentication
 ========================================== */
 
-const API_URL = "https://virtual-interior-designer-jiuv.onrender.com/api/auth";
+const API_URL = "https://virtual-interior-designer-vqfn.onrender.com";
 
 /*const API_URL = "http://localhost:5001/api/auth";*/
 // Change this to your Render/Railway URL after deployment
